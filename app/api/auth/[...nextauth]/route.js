17ef8f48e402850d03ded7afb29d36c007762fcc
Kareem moore <moorekareem877@gmail.com>
2025-06-09 11:29:02 -0400

    testing time

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,7 +14,6 @@ const handler = NextAuth({
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-          redirect_uri: "https://capstone-six-tau.vercel.app/api/auth/callback/google",
           redirect_uri: "https://capstone-six-tau.vercel.app/student/polypad"
         }
       },
